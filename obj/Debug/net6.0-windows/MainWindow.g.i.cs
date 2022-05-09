5fe252b3ba96084521a49f06a41cbe0f3ac1d125
nhutluong <Nhut.Luong@mscoding.org>
2022-05-09 11:10:09 -0500

    added powerpoint

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace SecretMessageEncoder {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SecretMessageEncoder;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SecretMessageEncoder;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
